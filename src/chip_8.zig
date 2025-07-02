b34e0d93b25904579b254033f26e91042fe9ecca
Filippo Menchini <33838929+filippomenchini@users.noreply.github.com>
2025-07-02 16:43:43 +0200

    execute

--- src/chip_8.zig
+++ src/chip_8.zig
@@ -1,13 +1,5 @@
-const Display = @import("display.zig");
-
-const Instruction = union(enum) {
-    clearScreen,
-    jump: u12,
-    setX: struct { register: u4, value: u8 },
-    addX: struct { register: u4, value: u8 },
-    setI: u12,
-    draw: struct { vx: u4, vy: u4, value: u4 },
-};
+const DISPLAY_WIDTH = 64;
+const DISPLAY_HEIGHT = 32;
 
 const FONTSET = [_]u8{
     0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
@@ -29,7 +21,7 @@ const FONTSET = [_]u8{
 };
 
 memory: [4096]u8,
-display: Display,
+display: [DISPLAY_WIDTH * DISPLAY_HEIGHT]bool,
 pc: u16,
 i: u16,
 stack: [16]u16,
@@ -41,7 +33,7 @@ registers: [16]u8,
 pub fn init() @This() {
     var chip_8 = @This(){
         .memory = [_]u8{0} ** 4096,
-        .display = Display.init(),
+        .display = [_]bool{false} ** (DISPLAY_WIDTH * DISPLAY_HEIGHT),
         .pc = 0x200,
         .i = 0,
         .stack = [_]u16{0} ** 16,
@@ -70,6 +62,15 @@ fn getNibble(raw: u16, position: u2) u4 {
     return @truncate((raw >> shift) & 0xF);
 }
 
+const Instruction = union(enum) {
+    clearScreen,
+    jump: u12,
+    setX: struct { register: u4, value: u8 },
+    addX: struct { register: u4, value: u8 },
+    setI: u12,
+    draw: struct { vx: u4, vy: u4, value: u4 },
+};
+
 const DecodeError = error{InvalidInstruction};
 fn decode(raw: u16) DecodeError!Instruction {
     if (raw == 0x00E0) return Instruction.clearScreen;
@@ -89,4 +90,61 @@ fn decode(raw: u16) DecodeError!Instruction {
         0xD => Instruction{ .draw = .{ .vx = x, .vy = y, .value = n } },
         else => DecodeError.InvalidInstruction,
     };
+}
+
+fn clearDisplay(self: *@This()) void {
+    self.display = [_]bool{false} ** (DISPLAY_WIDTH * DISPLAY_HEIGHT);
+}
+
+fn togglePixel(self: *@This(), x: u6, y: u5) void {
+    const index = y * DISPLAY_WIDTH + x;
+    self.display[index] = !self.display[index];
+}
+
+fn getPixel(self: *@This(), x: u6, y: u5) bool {
+    const index = y * DISPLAY_WIDTH + x;
+    return self.display[index];
+}
+
+fn execute(self: *@This(), instruction: Instruction) void {
+    switch (instruction) {
+        .jump => |address| {
+            self.pc = address;
+            return;
+        },
+        .clearScreen => self.clearDisplay(),
+        .setI => |i| self.i = i,
+        .setX => |data| self.registers[data.register] = data.value,
+        .addX => |data| self.registers[data.register] += data.value,
+        .draw => |data| {
+            const x = self.registers[data.vx] & (DISPLAY_WIDTH - 1);
+            const y = self.registers[data.vy] & (DISPLAY_HEIGHT - 1);
+
+            self.registers[0xF] = 0;
+
+            for (0..data.value) |i| {
+                const current_y = y + i;
+                const sprite = self.memory[self.i + i];
+
+                for (0..8) |j| {
+                    const pixel_bit = (sprite >> (7 - j)) & 1;
+                    const current_x = x + j;
+
+                    if (pixel_bit != 0) {
+                        if (current_x < DISPLAY_WIDTH and current_y < DISPLAY_HEIGHT) {
+                            const screen_pixel_was_on = self.getPixel(current_x, current_y);
+
+                            self.togglePixel(current_x, current_y);
+
+                            if (screen_pixel_was_on) {
+                                self.registers[0xF] = 1;
+                            }
+                        }
+                    }
+                }
+            }
+        },
+    }
+
+    self.pc += 2;
 }
--- src/display.zig
+++
@@ -1,9 +0,0 @@
-rows: [64]bool,
-cols: [32]bool,
-
-pub fn init() @This() {
-    return @This(){
-        .rows = [_]bool{false} ** 64,
-        .cols = [_]bool{false} ** 32,
-    };
-}
